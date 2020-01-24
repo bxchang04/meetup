f6a3c3dfb813b3dd39035a5ea31af1ac811a2ef2
bxchang04 <42091866+bxchang04@users.noreply.github.com>
2020-01-24 09:51:17 -0500

    update auth handler.js

--- auth-server_OLD/handler.js
+++ auth-server_OLD/handler.js
@@ -9,13 +9,16 @@ module.exports.getAccessToken = async (event) => {
     + '&client_secret=v1A/UF3x+u2vzYrj5AsIS5frj+Z9gmdPCgd6qSsy'
     + '&grant_type=authorization_code'
     + '&redirect_uri=https://bxchang04.github.io/meetup'
-    + '&code=5ae54fbcd365973cc7c502463cb42fd8';
+    + '&code=5f611a77f6f9b64652a8567d53a02f01';
     + '&code=' + event.pathParameters.code;
 
   const info = await axios.post(MEETUP_OAUTH_URL);
 
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
     body: JSON.stringify({
       access_token: info.data.access_token,
       refresh_token: info.data.refresh_token,
